d97fa2a3ac89a75160712151415befd5a55186d7
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-02-12 00:09:12 -0300

    getting and inserting tickers functionalities

--- clearScript.py
+++ clearScript.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import optionCodes
 import time
 
 chrome_options = Options()
@@ -9,12 +12,9 @@ chrome_driver = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 driver = webdriver.Chrome(chrome_options)
 driver.get('https://portal.clear.com.br/login')
 actions = ActionChains(driver)
+input('')
 
-def executeNewAssetInfoFlow(ticker):
-    actions.move_by_offset(450,140)
-    actions.click()
-    actions.perform()
-    actions.move_by_offset(-450,-140)
+def newAssetFlow(ticker):
     actions.move_by_offset(400,65)
     actions.click()
     actions.perform()
@@ -23,31 +23,27 @@ def executeNewAssetInfoFlow(ticker):
     actions.click()
     actions.perform()
     e = driver.find_element(By.CSS_SELECTOR, '[placeholder="Novo ativo"]')
-    e.send_keys(ticker[0])
-    time.sleep(0.05)
-    e.send_keys(ticker[1])
-    time.sleep(0.05)
-    e.send_keys(ticker[2])
-    time.sleep(0.05)
-    e.send_keys(ticker[3])
-    time.sleep(0.05)
-    e.send_keys(ticker[4])
-    time.sleep(0.05)
+    [(e.send_keys(ticker[i]),time.sleep(0.05)) for i in range(0,len(ticker))]
     e.send_keys(' ')
     actions.move_by_offset(-610,-215)
-    actions.move_by_offset(620,290)
-    time.sleep(2)
-    actions.click()
-    actions.perform()
-    actions.move_by_offset(-620,-290)
+    time.sleep(1.5)
+    e = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[class='small soma-caption ellipsis hydrated']"))
+    )
+    e.click()
     actions.move_by_offset(931,543)
     actions.click()
     actions.perform()
     actions.move_by_offset(-931, -543)
     actions.click()
     actions.perform()
+
+def addAssets():
+    [newAssetFlow(i) for i in optionCodes.get()]
     time.sleep(0.5)
     prices = [i.text for i in driver.find_elements(By.CSS_SELECTOR, '[class="defaultCell bodyCell"]')[8:]]
     print(prices)
 
-executeNewAssetInfoFlow('VBBR3')
+addAssets()
+
+input()
---
+++ coordinates.js
@@ -0,0 +1,5 @@
+document.addEventListener("mousemove", function (event) {
+  console.clear(); // Clear console for better readability
+  console.log("Mouse X:", event.clientX);
+  console.log("Mouse Y:", event.clientY);
+});
