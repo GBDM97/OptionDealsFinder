69eb797e7f296f89a40a56e27367c2d96e2d9336
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-02-25 23:39:20 -0300

    generateDataSet algorithm improved

--- clearScript.py
+++ clearScript.py
@@ -7,26 +7,45 @@ def testPrices():
     with open('Data\\testPrices.json', 'r') as file:
         return ast.literal_eval(file.read().replace('null','None'))
 
-def generateDataSet(driver) -> list[list[dict]]:
+def generateDataSet() -> list[list[dict]]:
     asset_dataset = []
     final_dataset = []
+
+    def save(readArray,writeDict):
+        writeDict['buyPrice'] = readArray['arguments'][1]['bestBuyPrice']
+        writeDict['sellPrice'] = readArray['arguments'][1]['bestSellPrice']
+        asset_dataset.append(writeDict)
+
+    def search(readArray,writeDict):
+        for i in readArray:
+            if i['arguments'][0] == writeDict['code']:
+                save(readArray,writeDict)
+                return True
+        return False
+            
     prices_index = 0
     prices = testPrices()
     # prices = ws.queryPrices(driver)
-    for ii in optionCodes.get():
+    for i in optionCodes.get():
         asset_dataset = []
-        if prices_index >= 7000:
-            print
-        for i in ii:
-            b = prices[prices_index]['arguments'][1]['bestBuyPrice'] 
-            s = prices[prices_index]['arguments'][1]['bestSellPrice']
-            new = {'code': i['code'], 'buyPrice': b if b!=None else '-','sellPrice': s if s!=None else '-'}
-            if 'strike' in i:
-                new['strike'] = i['strike']
-            asset_dataset.append(new)
-            prices_index += 1
-        final_dataset.append(asset_dataset)
+        for ii in i:
+            if ii['code'] == prices[prices_index]['arguments'][0]:
+                save(prices[prices_index],ii)
+            elif 'strike' not in ii:
+                if not search(prices[prices_index-50:prices_index+50],ii):
+                    while ii['code'][:4] == prices[prices_index]['arguments'][0][:4]:
+                        prices_index+=1
+                    break
+            else:
+                if not search(prices[prices_index-50:prices_index+50],ii):
+                    prices_index+=1
+                    continue
+            prices_index+=1
+        if asset_dataset:
+            final_dataset.append(asset_dataset)
     return final_dataset
 
 def getLockOutput(driver):
     return dataProcess.getLockInfo(generateDataSet(driver))
+
+generateDataSet()
