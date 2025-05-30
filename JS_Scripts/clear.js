900f40e2f530d7af9d33d4f2296f7e563dc90845
Ghiancarlo Morelli <ghian.m@gmail.com>
2025-05-30 20:23:43 -0300

    clear.js functional improvements under way

--- JS_Scripts/clear.js
+++ JS_Scripts/clear.js
@@ -1,12 +1,12 @@
+const toFloat = (val, decimals = 2) => {
+  if((typeof val) !== 'string' && (typeof val) !== 'number' || val === '-' || !val ) return NaN;
+  return parseFloat(parseFloat(val.toString().replace(',', '.')).toFixed(decimals));
+}
+
 const extractOptions = () => {
-  const rows = document.evaluate("/html/body/div[1]/soma-context/div/div[3]/div/div[3]/arsenal-loader/div/div[5]/table/tbody",
-                                  document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.rows;
+  const rows = Array.from(document.querySelectorAll('tbody'))[1].querySelectorAll("tr");
   const options = [];
-  const toFloat = (val, decimals = 2) => {
-    if((typeof val) !== 'string' && (typeof val) !== 'number' || val === '-' || !val ) return NaN;
-    return parseFloat(parseFloat(val.toString().replace(',', '.')).toFixed(decimals));
-  }
-  Array.from(rows).forEach(row => {
+  rows.forEach(row => {
     const cells = Array.from(row.querySelectorAll("td")).map(v=>v.querySelector("td soma-caption").textContent.trim());
     const strike = toFloat(cells[5]);
     const callBid = toFloat(cells[3]);
@@ -25,69 +25,156 @@ const extractOptions = () => {
   return options.sort((a, b) => a.strike - b.strike);
 };
 
-const generateVerticalSpreads = (options, useMid) => {
+const generateVerticalSpreads = (options, useMid, listType, price) => {
     const results = [];
 
     for (let i = 0; i < options.length; i++) {
         for (let j = 0; j < options.length; j++) {
-        if (i === j) continue;
-        const o1 = options[i];
-        const o2 = options[j];
-        
-        let long;
-        let short;
+          const o1 = options[i];
+          const o2 = options[j];
 
-        // if(o1.type === 'CALL'){
-        //     long = 
-        //     short = 
-        // }
+          if (i === j) continue;
+          if(o1.strike > o2.strike) continue;
+          let long;
+          let short;
 
-        // const longPrice = useMid ? long.mid : long.ask;
-        // const shortPrice = useMid ? short.mid : short.bid;
+          if(listType === 'OTMCalls'|| listType === 'ITMPuts'){
+            short = o1
+            long = o2
+          }
 
-        // const spreadWidth = Math.abs(short.strike - long.strike);
-        // const cost = longPrice - shortPrice;
-        // const isDebit = cost > 0;
+          if(listType === 'ITMCalls'|| listType === 'OTMPuts'){
+            short = o2
+            long = o1
+          }
 
-        // const maxProfit = isDebit ? spreadWidth - cost : cost;
-        // const maxLoss = isDebit ? cost : spreadWidth - cost;
+          const longPrice = useMid ? long.mid : long.ask;
+          const shortPrice = useMid ? short.mid : short.bid;
 
-        // if (maxLoss <= 0) continue;
+          const spreadWidth = Math.abs(short.strike - long.strike);
+          const cost = longPrice - shortPrice;
+          const isDebit = cost > 0;
 
-        // results.push({
-        //     type: isDebit ? 'DebitSpread' : 'CreditSpread',
-        //     optionType: long.type,
-        //     longStrike: long.strike,
-        //     shortStrike: short.strike,
-        //     cost: +cost.toFixed(2),
-        //     maxProfit: +maxProfit.toFixed(2),
-        //     maxLoss: +maxLoss.toFixed(2),
-        //     gainPercentage: +(maxProfit / maxLoss).toFixed(2),
-        // });
-        }
+          const maxProfit = isDebit ? spreadWidth - cost : cost *-1;
+          const maxLoss = isDebit ? cost : (spreadWidth + cost);
+          const gainPercentage = (maxProfit / maxLoss)*100;
+          const distance = Math.abs(price - short.strike);
+
+          if(listType === 'OTMCalls' && isDebit) continue;
+          if(listType === 'OTMPuts' && isDebit) continue;
+          if(maxProfit <= 0) continue;
+          
+          results.push({
+              type: isDebit ? 'DebitSpread' : 'CreditSpread',
+              gainPercentage: +(gainPercentage).toFixed(2),
+              optionType: long.type + (listType === "OTMCalls" || listType === 'OTMPuts' ? " - OTM" : " - ITM"),
+              shortStrike: short.strike,
+              cost: +cost.toFixed(2),
+              maxProfit: +maxProfit.toFixed(2),
+              maxLoss: +maxLoss.toFixed(2),
+              index: gainPercentage * distance,
+              additionalInformation :{
+                maxProfit: +maxProfit.toFixed(2),
+                maxLoss: +maxLoss.toFixed(2),
+                spreadWidth,
+                distance,
+                longStrike: long.strike,
+                long,
+                short
+              }
+          });
+          }
     }
-
-    return results;
+    return results.sort((a, b) => b.gainPercentage - a.gainPercentage);
 };
 
-const find = (lowerResistance, upperResistance, useMid = true) => {
-    let options = extractOptions();
-    const lowerLimit = parseFloat(lowerResistance);
-    const upperLimit = parseFloat(upperResistance);
+const generateStructures = (spreads) => {
+  ironCondors = [];
+  hybrids = [];
+  for (let i = 0; i < spreads.length; i++) {
+    for (let j = 0; j < spreads.length; j++) {
+      const s1 = spreads[i];
+      const s2 = spreads[j];
+      let hybrid = false;
+      if (i === j) continue;
+      if (s1.optionType === s2.optionType) continue;
+      if (s1.shortStrike === s2.shortStrike) continue;
+
+      if((s1.optionType === 'CALL - OTM' && s2.optionType === 'PUT - ITM')||
+         (s1.optionType === 'CALL - ITM' && s2.optionType === 'PUT - OTM')||
+         (s2.optionType === 'CALL - OTM' && s1.optionType === 'PUT - ITM')||
+         (s2.optionType === 'CALL - ITM' && s1.optionType === 'PUT - OTM')){
+            hybrid = true;
+         }
+      const s1p = s1.additionalInformation.maxProfit;
+      const s2p = s2.additionalInformation.maxProfit;
+      const s1l = s1.additionalInformation.maxLoss;
+      const s2l = s2.additionalInformation.maxLoss;
+      const structureMaxProfit = s1p + s2p;
+      const structureMaxLoss = hybrid ? s1l + s2l : Math.min(s1p - s2l, s2p - s1l) *-1;
+      const structureGainPercentage = (structureMaxProfit / structureMaxLoss) *100;
+      const distance = Math.min(s1.additionalInformation.distance, s2.additionalInformation.distance);
+      const index = (distance*25) + structureGainPercentage;
+
+      if(structureGainPercentage < 20) continue;
+      
+      if(hybrid) {
+        hybrids.push({
+          gainPercentage: structureGainPercentage,
+          structureMaxProfit,
+          structureMaxLoss,
+          index,
+          s1,
+          s2
+        })
+      } else {
+        ironCondors.push({
+          gainPercentage: structureGainPercentage,
+          structureMaxProfit,
+          structureMaxLoss,
+          index,
+          s1,
+          s2
+        })
+      }
+      
+    }
+  }
+  hybrids.sort((a, b) => b.gainPercentage - a.gainPercentage);
+  ironCondors.sort((a, b) => b.index - a.index);
+  return {ironCondors,hybrids};
+}
+
+const find = (useMid = false, lowerResistance, upperResistance) => {
+    let options = temp1;
+    const price = Array.from(document.querySelector('tbody')
+                                     .querySelector('tr')
+                                     .querySelectorAll('td'))[2]
+                                     .querySelector("td soma-caption").textContent.trim();
+    const lowerLimit = toFloat(lowerResistance || price);
+    const upperLimit = toFloat(upperResistance || price);
     const calls = options.filter(v => v.type === 'CALL');
-    const upperCalls = calls.filter(o => o.strike >= upperLimit);
-    const lowerCalls = calls.filter(o => o.strike <= lowerLimit);
+    const OTMCalls = calls.filter(o => o.strike >= upperLimit);
+    const ITMCalls = calls.filter(o => o.strike <= lowerLimit);
     const puts = options.filter(v => v.type === 'PUT');
-    const upperPuts = puts.filter(o => o.strike >= upperLimit);
-    const lowerPuts = puts.filter(o => o.strike <= lowerLimit);
+    const ITMPuts = puts.filter(o => o.strike >= upperLimit);
+    const OTMPuts = puts.filter(o => o.strike <= lowerLimit);
 
-    const spreads = generateVerticalSpreads(upperCalls, useMid);
-    // const condors = generateIronCondors(options, useMid);
-    // const all = [...spreads, ...condors].sort((a, b) => b.gainPercentage - a.gainPercentage);
+    const OTMCallsSpreads = generateVerticalSpreads(OTMCalls, useMid, 'OTMCalls', upperLimit);
+    const ITMCallsSpreads = generateVerticalSpreads(ITMCalls, useMid, 'ITMCalls', lowerLimit);
+    const ITMPutsSpreads = generateVerticalSpreads(ITMPuts, useMid, 'ITMPuts', upperLimit);
+    const OTMPutsSpreads = generateVerticalSpreads(OTMPuts, useMid, 'OTMPuts', lowerLimit);
+    const spreads = [...OTMCallsSpreads, ...ITMCallsSpreads, ...ITMPutsSpreads, ...OTMPutsSpreads];
+    const structures = generateStructures(spreads);
+    const all = [...structures.ironCondors, ...spreads].sort((a, b) => b.index - a.index);
 
-    console.log("Top 10 Combinations:");
-    // console.log(JSON.stringify(all.slice(0, 10), null, 2));
+    console.dir({all, ironCondors: structures.ironCondors, hybrids: structures.hybrids, spreads});
 };
 
-find(130, 138, false);
+find();
 
+//iterar por faixas de porcentagem de 1% pegando a melhor operação por distância
+  //ordenar por porcentagem e iterar a lista, salva a primeira distancia, se encontrar uma distância maior sobreescreve, terminou a faixa, registra e vai pra próxima
+//teste uma situação onde se encontraria travas de credito itm, pois são oportunidades de arbitragem
+//pense em alguma verificação de distância da trava
+//teste se os preços não estão sendo modificados pela ineficiência do js em calculos
